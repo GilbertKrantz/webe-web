ba955ad248feb04829868f2f7802f6501c55558a
GilbertKrantz <wilbertchandra.official@gmail.com>
2026-02-01 01:13:43 +0700

    [FIX] Configure Astro output mode to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
     integrations: [react(), tailwind({
         applyBaseStyles: false,
     })],
-    output: 'hybrid',
+    output: 'static',
     adapter: vercel(),
 });
