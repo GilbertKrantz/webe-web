b366aac5f8fa7b68b419bb28085b98ef4140b0d1
Wilbert Chandra <wilbertchandra.official@gmail.com>
2026-02-02 17:03:43 +0000

    fix: register astro-icon integration in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind({
         applyBaseStyles: false,
-    })],
+    }), icon()],
     output: 'server',
     adapter: vercel(),
     vite: {
